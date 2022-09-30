558f723b9052feacad4e7416bd57ba27a1406eab
Debasish Sahoo <dsahoo@nexient.com>
2022-09-30 11:14:17 -0400

    Updated git sources from ssh to https

--- main.tf
+++ main.tf
@@ -103,7 +103,7 @@ module "service_plan" {
 }
 
 module "web_app" {
-  source = "git@github.com:nexient-llc/tf-azurerm-module-linux_web_app.git?ref=0.1.0"
+  source = "github.com/nexient-llc/tf-azurerm-module-linux_web_app.git?ref=0.1.0"
 
   for_each = toset(var.backend_regions)
 
@@ -147,7 +147,7 @@ module "web_app" {
 }
 
 module "front_door" {
-  source = "git@github.com:nexient-llc/tf-azurerm-module-front_door.git?ref=0.1.0"
+  source = "github.com/nexient-llc/tf-azurerm-module-front_door.git?ref=0.1.0"
   count  = var.front_door_enabled ? 1 : 0
 
   resource_group            = local.resource_groups[local.primary_region]
